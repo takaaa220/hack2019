beae6dc35f4c97656b60c4cfe980ef2b2d3b37bd
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-03-16 22:25:15 +0900

    create post and update action

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,23 +5,37 @@ class ShopsController < ApplicationController
   end
 
   def create
-    user = User.find_by(uid: paams[:uid])
-    shop = Shop.new(shpp_params)
+    unless user = User.find_by(uid: params[:uid])
+      return render status: 401, json: { messages: "not login" }
+    end
+
+    shop = user.shops.build(shop_params)
     if shop.save
-      render json: { status: 200, data: res_model(shop) }
+      render json: { data: res_model(shop) }
     else
-      render json: { status: 400, data: res_model(shop), message: shop.errors.messages }
+      render status: 400, json: { data: res_model(shop), message: shop.errors.full_messages }
     end
   end
 
   def update
+    user = User.find_by(uid: params[:uid])
+    shop = user&.shops&.find_by(id: params[:id])
+    if user.nil? || shop.nil?
+      return render status: 401, json: { messages: "unauthorized access" }
+    end
+
+    if shop.update(shop_params)
+      render json: { data: res_model(shop) }
+    else
+      render status: 400, json: { data: res_model(shop), message: shop.errors.full_messages }
+    end
   end
 
 
   private
 
   def shop_params
-    params.permit!(:name, :description, :category)
+    params.permit(:name, :description, :category)
   end
 
   def res_model(shop)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :shops, only: %i(index)
+  resources :shops, only: %i(index create update)
   resources :users, only: %i(index show)
   get '/auth/twitter/callback' => 'sessions#callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
